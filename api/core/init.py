21731f9607eeb520736452550bfea5392fbca822
ArtemisFowl24 <tahers7@mcmaster.ca>
2025-11-04 22:25:52 -0500

    Removed web scraping for now, added baseline model structure

--- api/core/init.py
+++
@@ -1,6 +0,0 @@
-"""DEPRECATED package: `api.core` moved to `api` subpackages such as
-`api.webscraping` and `api.claim_verification`.
-
-Keep this module only as a short-lived compatibility layer. Remove once
-you're confident no code imports from `api.core`.
-"""
--- api/core/scraping/init.py
+++
@@ -1,9 +0,0 @@
-"""DEPRECATED package: `api.core.scraping` moved to `api.webscraping`.
-
-This package now re-exports the new implementation. Remove this package
-once all imports have been migrated to `api.webscraping`.
-"""
-
-from api.webscraping.fetch import fetch, FetchResult, canonicalize_url
-
-__all__ = ["fetch", "FetchResult", "canonicalize_url"]
