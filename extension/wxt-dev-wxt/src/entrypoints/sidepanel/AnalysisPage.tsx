cef4824260b722530c933afc812a012d7e85df42
Louis Doan <louisdoan9@gmail.com>
2025-11-20 07:58:27 -0500

    cleanup code

--- extension/wxt-dev-wxt/src/entrypoints/sidepanel/AnalysisPage.tsx
+++ extension/wxt-dev-wxt/src/entrypoints/sidepanel/AnalysisPage.tsx
@@ -1,18 +1,10 @@
-import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Progress } from '@/components/ui/progress';
 import { Tabs, TabsContent, TabsList } from '@/components/ui/tabs';
 import { TabsTrigger } from '@radix-ui/react-tabs';
-import {
-	CheckCircleIcon,
-	CircleXIcon,
-	ShieldIcon,
-	TextIcon,
-	TrendingUpDownIcon,
-	TriangleAlertIcon,
-} from 'lucide-react';
+import { TextIcon } from 'lucide-react';
 import BiasTab from './BiasTab';
+import ClaimTab from './ClaimTab';
 
 interface AnalysisResult {
 	checks: number;
@@ -32,21 +24,21 @@ export default function AnalysisPage({
 	setText,
 	result,
 	setResult,
-	data,
 }: {
 	text: string | undefined;
 	setText: React.Dispatch<React.SetStateAction<string | undefined>>;
 	result: AnalysisResult | undefined;
 	setResult: React.Dispatch<React.SetStateAction<AnalysisResult | undefined>>;
-	data: any;
 }) {
 	const [currentHovered, setCurrentHovered] = useState<string>();
+	const [currentTab, setCurrentTab] = useState<string>('bias');
 
 	// highlights, scroll to claim on sidepanel
 	useEffect(() => {
 		const highlightHoveredText = (message: any) => {
 			if (message.type === 'UNDERLINE_HOVER') {
 				setCurrentHovered(message.text);
+				console.log(message.text);
 				const element = document.querySelector(`[claim-text*="${CSS.escape(message.text)}"]`);
 				if (element) {
 					element.scrollIntoView({ behavior: 'smooth', block: 'center' });
@@ -78,12 +70,18 @@ export default function AnalysisPage({
 			await browser.tabs.sendMessage(tab.id ?? 0, {
 				type: 'UNDERLINE_SELECTION',
 				valid: false,
-				targets: data?.bias_claims.filter((x) => !x.valid).map((x) => x.text),
+				targets:
+					currentTab === 'bias'
+						? result?.bias_claims.filter((x) => !x.valid).map((x) => x.text)
+						: result?.fact_check_claims.filter((x) => !x.valid).map((x) => x.text),
 			});
 			await browser.tabs.sendMessage(tab.id ?? 0, {
 				type: 'UNDERLINE_SELECTION',
 				valid: true,
-				targets: data?.bias_claims.filter((x) => x.valid).map((x) => x.text),
+				targets:
+					currentTab === 'bias'
+						? result?.bias_claims.filter((x) => x.valid).map((x) => x.text)
+						: result?.fact_check_claims.filter((x) => x.valid).map((x) => x.text),
 			});
 		}
 	}
@@ -99,6 +97,41 @@ export default function AnalysisPage({
 		await browser.tabs.sendMessage(tab.id ?? 0, {
 			type: 'CLEAR_UNDERLINES',
 		});
+	}
+
+	async function switchTab(value: string) {
+		const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+		if (!tab?.id) return;
+
+		if (value === 'claims') {
+			await browser.tabs.sendMessage(tab.id, {
+				type: 'CLEAR_UNDERLINES',
+			});
+			await browser.tabs.sendMessage(tab.id ?? 0, {
+				type: 'UNDERLINE_SELECTION',
+				valid: false,
+				targets: result?.fact_check_claims.filter((x) => !x.valid).map((x) => x.text),
+			});
+			await browser.tabs.sendMessage(tab.id ?? 0, {
+				type: 'UNDERLINE_SELECTION',
+				valid: true,
+				targets: result?.fact_check_claims.filter((x) => x.valid).map((x) => x.text),
+			});
+		} else {
+			await browser.tabs.sendMessage(tab.id, {
+				type: 'CLEAR_UNDERLINES',
+			});
+			await browser.tabs.sendMessage(tab.id ?? 0, {
+				type: 'UNDERLINE_SELECTION',
+				valid: false,
+				targets: result?.bias_claims.filter((x) => !x.valid).map((x) => x.text),
+			});
+			await browser.tabs.sendMessage(tab.id ?? 0, {
+				type: 'UNDERLINE_SELECTION',
+				valid: true,
+				targets: result?.bias_claims.filter((x) => x.valid).map((x) => x.text),
+			});
+		}
 	}
 
 	return (
@@ -121,7 +154,14 @@ export default function AnalysisPage({
 				</CardContent>
 			</Card>
 
-			<Tabs defaultValue="bias" className="max-w-xs w-full gap-4">
+			<Tabs
+				defaultValue="bias"
+				className="max-w-xs w-full gap-4"
+				onValueChange={(e) => {
+					setCurrentTab(e);
+					switchTab(e);
+				}}
+			>
 				<TabsList className="w-full h-fit flex justify-around p-2">
 					<TabsTrigger
 						value="bias"
@@ -137,42 +177,20 @@ export default function AnalysisPage({
 					</TabsTrigger>
 				</TabsList>
 				<TabsContent value="bias" className="flex flex-col gap-4">
-					<BiasTab result={result} currentHovered={currentHovered} handleHighlight={handleHighlight} />
+					<BiasTab
+						key={currentTab}
+						result={result}
+						currentHovered={currentHovered}
+						handleHighlight={handleHighlight}
+					/>
 				</TabsContent>
-				{/* Mocking claim verification */}
 				<TabsContent value="claims">
-					<Card>
-						<CardHeader className="flex gap-2 items-center">
-							<ShieldIcon size={20} />
-							<p className="font-semibold text-base">Claim Verification</p>
-						</CardHeader>
-						<CardContent className="flex flex-col gap-2">
-							<div className="bg-yellow-50 border border-yellow-200 rounded p-2 mb-2">
-								<p className="text-xs text-yellow-800">This section shows mock data for now.</p>
-							</div>
-							{result?.fact_check_claims.map((claim, idx) => (
-								<div
-									key={`fact-${idx}`}
-									className={`bg-gray-50 flex rounded-b-sm gap-4 items-center py-2 pl-4 hover:cursor-pointer border-2 border-white ${
-										currentHovered === claim.text && 'border-yellow-200'
-									}`}
-									claim-text={claim.text}
-									onMouseEnter={() => handleHighlight(claim.text)}
-									onMouseLeave={() => handleHighlight('')}
-								>
-									{claim.valid ? (
-										<CheckCircleIcon className="w-12 h-12 text-green-400" />
-									) : (
-										<TriangleAlertIcon className="w-12 h-12 text-red-400" />
-									)}
-									<div className="flex flex-col">
-										<h3 className="text-sm">{claim.category}</h3>
-										<p className="text-xs text-gray-400">{claim.description}</p>
-									</div>
-								</div>
-							))}
-						</CardContent>
-					</Card>
+					<ClaimTab
+						key={currentTab}
+						result={result}
+						currentHovered={currentHovered}
+						handleHighlight={handleHighlight}
+					/>
 				</TabsContent>
 			</Tabs>
 		</CardContent>
