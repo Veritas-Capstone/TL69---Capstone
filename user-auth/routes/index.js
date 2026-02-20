f11df5fdfd45b82ef00ae8af96a7179a685baa73
Louis Doan <louisdoan9@gmail.com>
2026-02-19 20:56:36 -0500

    create simple routes for DB

--- user-auth/routes/index.js
+++ user-auth/routes/index.js
@@ -1,9 +1,89 @@
 var express = require('express');
 var router = express.Router();
+var userSchema = require('../schemas/users');
+var statsSchema = require('../schemas/stats');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Veritas DB' });
+});
+
+router.post('/create-user', function (req, res, next) {
+	async function getUser() {
+		return await userSchema.find({ username: req.body.username });
+	}
+
+	getUser().then((user) => {
+		if (user.length === 0) {
+			const newUser = new userSchema({
+				username: req.body.username,
+				password: req.body.password,
+			});
+			const newStats = new statsSchema({
+				username: req.body.username,
+				leftBiasNum: 0,
+				rightBiasNum: 0,
+				centerBiasNum: 0,
+			});
+
+			newUser
+				.save()
+				.then((savedUser) => {
+					return newStats.save().then(() => savedUser);
+				})
+				.then((savedUser) => {
+					res.json(savedUser);
+				})
+				.catch((err) => {
+					console.error(err);
+					res.json({ success: false, error: err.message });
+				});
+		} else {
+			res.json({ success: false });
+		}
+	});
+});
+
+router.post('/user', async function (req, res, next) {
+	async function getUser() {
+		return await userSchema.findOne({ username: req.body.username, password: req.body.password });
+	}
+
+	getUser().then((user) => {
+		res.json(user);
+	});
+});
+
+router.post('/get-stats', async function (req, res, next) {
+	async function getStats() {
+		return await statsSchema.findOne({ username: req.body.username });
+	}
+
+	getStats().then((stats) => {
+		res.json(stats);
+	});
+});
+
+router.post('/stats', async function (req, res, next) {
+	try {
+		const { username, leftBias, rightBias, centerBias } = req.body;
+
+		const updatedStats = await statsSchema.findOneAndUpdate(
+			{ username },
+			{
+				$inc: {
+					leftBiasNum: leftBias,
+					rightBiasNum: rightBias,
+					centerBiasNum: centerBias,
+				},
+			},
+			{ new: true, upsert: true },
+		);
+
+		res.json({ success: true, stats: updatedStats });
+	} catch (err) {
+		console.error(err);
+		res.json({ success: false, error: err.message });
+	}
 });
 
 module.exports = router;
---
+++ user-auth/schemas/stats.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Stats = new Schema({
+	username: { type: String, required: true },
+	leftBiasNum: { type: Number, required: true },
+	rightBiasNum: { type: Number, required: true },
+	centerBiasNum: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Stats', Stats);
